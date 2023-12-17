1aa68425c8b0955bb98bb496989390999277d218
igorivnaolga <109050006+igorivnaolga@users.noreply.github.com>
2023-12-17 11:21:02 +0200

    add task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeColorBtn = document.querySelector('.change-color');
+const page = document.querySelector('body');
+const color = document.querySelector('.color');
+
+const handleClick = (event) => {
+  const randomColor = getRandomHexColor();
+  color.textContent = randomColor;
+  page.style.backgroundColor = randomColor;
+};
+changeColorBtn.addEventListener('click', handleClick);
--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,28 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+// const input = document.querySelector('input');
+// const create = document.querySelector('[data-create]');
+// const destroy = document.querySelector('[data-destroy]');
+// const boxes = document.querySelector('#boxes');
+// const amount = input.value;
+// function createBoxes(amount) {
+//   for (let i = 1; i <= amount; i++) {
+//     let box = '<div></div>';
+//     box.style.width = '10';
+//     box.style.height = '10';
+//     box.style.color = 'getRandomHexColor()';
+//   }
+// }
+
+// const handleCreate = (event) => {
+//   createBoxes();
+// };
+
+// const handleDestroy = (event) => {
+//   boxes.innerHTML = '';
+// };
+
+// create.addEventListener('click', handleCreate);
+// destroy.addEventListener('click', handleDestroy);
