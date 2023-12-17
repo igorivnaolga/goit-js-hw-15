4a98405e5f70c3d72aa2166334f8115ddd53a751
igorivnaolga <109050006+igorivnaolga@users.noreply.github.com>
2023-12-17 19:21:05 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -4,27 +4,36 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
-// const input = document.querySelector('input');
-// const create = document.querySelector('[data-create]');
-// const destroy = document.querySelector('[data-destroy]');
-// const boxes = document.querySelector('#boxes');
-// const amount = input.value;
-// function createBoxes(amount) {
-//   for (let i = 1; i <= amount; i++) {
-//     let box = '<div></div>';
-//     box.style.width = '10';
-//     box.style.height = '10';
-//     box.style.color = 'getRandomHexColor()';
-//   }
-// }
+const input = document.querySelector('input');
+const create = document.querySelector('[data-create]');
+const destroy = document.querySelector('[data-destroy]');
+const boxes = document.querySelector('#boxes');
+const amount = input.value;
+let width = 30;
+let height = 30;
 
-// const handleCreate = (event) => {
-//   createBoxes();
-// };
+function createBoxes(amount) {
+  for (let i = 1; i <= amount; i++) {
+    let box = document.createElement('div');
+    box.style.width = width + 'px';
+    box.style.height = height + 'px';
+    box.style.backgroundColor = getRandomHexColor();
+    boxes.insertAdjacentElement('beforeend', box);
+    width += 10;
+    height += 10;
+  }
+}
 
-// const handleDestroy = (event) => {
-//   boxes.innerHTML = '';
-// };
+const handleCreate = (event) => {
+  const amount = input.value;
 
-// create.addEventListener('click', handleCreate);
-// destroy.addEventListener('click', handleDestroy);
+  createBoxes(amount);
+};
+
+const handleDestroy = (event) => {
+  boxes.innerHTML = '';
+  input.value = 0;
+};
+
+create.addEventListener('click', handleCreate);
+destroy.addEventListener('click', handleDestroy);
