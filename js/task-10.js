75be5b6f3cd7a9eebd8b22d3e3c38fd97dc296b1
igorivnaolga <109050006+igorivnaolga@users.noreply.github.com>
2023-12-18 19:34:02 +0200

    fix bugs task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -8,32 +8,34 @@ const input = document.querySelector('input');
 const create = document.querySelector('[data-create]');
 const destroy = document.querySelector('[data-destroy]');
 const boxes = document.querySelector('#boxes');
-const amount = input.value;
-let width = 30;
-let height = 30;
 
 function createBoxes(amount) {
+  let sizes = 30;
+  const elements = [];
   for (let i = 1; i <= amount; i++) {
     let box = document.createElement('div');
-    box.style.width = width + 'px';
-    box.style.height = height + 'px';
+    box.style.width = sizes + 'px';
+    box.style.height = sizes + 'px';
     box.style.backgroundColor = getRandomHexColor();
-    boxes.insertAdjacentElement('beforeend', box);
-    width += 10;
-    height += 10;
+    elements.push(box);
+    sizes += 10;
   }
+  boxes.append(...elements);
 }
 
 const handleCreate = (event) => {
   const amount = input.value;
-
+  destroyBoxes();
   createBoxes(amount);
 };
 
 const handleDestroy = (event) => {
-  boxes.innerHTML = '';
+  destroyBoxes();
   input.value = 0;
 };
+function destroyBoxes() {
+  boxes.innerHTML = '';
+}
 
 create.addEventListener('click', handleCreate);
 destroy.addEventListener('click', handleDestroy);
